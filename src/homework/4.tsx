d613b37c080d8c78e0f52f1ffdb19ec313edfdef
ishkov2709 <dmitryishkov27@gmail.com>
2024-01-04 22:08:58 +0200

    solved tasks

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -6,18 +6,32 @@ type Menu = { id: MenuIds; title: string };
 
 // Додати тип Menu Selected
 
+type MenuSelected = {
+  selectedMenu: {
+    id?: MenuIds;
+  };
+};
+
 const MenuSelectedContext = createContext<MenuSelected>({
   selectedMenu: {},
 });
 
 // Додайте тип MenuAction
 
+type MenuAction = {
+  onSelectedMenu: React.Dispatch<React.SetStateAction<SelectedMenu>>;
+};
+
 const MenuActionContext = createContext<MenuAction>({
   onSelectedMenu: noop,
 });
 
 type PropsProvider = {
-  children; // Додати тип для children
+  children: React.ReactNode;
+};
+
+type SelectedMenu = {
+  id?: MenuIds;
 };
 
 function MenuProvider({ children }: PropsProvider) {
@@ -48,7 +62,7 @@ function MenuProvider({ children }: PropsProvider) {
 }
 
 type PropsMenu = {
-  menus; // Додайте вірний тип для меню
+  menus: Menu[]; // Додайте вірний тип для меню
 };
 
 function MenuComponent({ menus }: PropsMenu) {
